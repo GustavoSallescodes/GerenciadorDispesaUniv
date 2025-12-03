f59cc73fabe29ffae2d51ef8f1af76e0bcdb760a
GustavoSallescodes <gustavo@gunet.com.br>
2025-12-02 21:00:29 -0300

    Update despesa_comum.hpp

--- despesa_comum.hpp
+++ despesa_comum.hpp
@@ -1,1 +1,24 @@
+#ifndef DESPESA_COMUM_HPP
+#define DESPESA_COMUM_HPP
+
+#include "despesa.hpp"
+#include <vector>
+
+class Morador;
+
+class DespesaComum : public Despesa {
+private:
+    std::vector<Morador*> moradores_envolvidos_;
+
+public:
+    DespesaComum(float valor,
+                 std::string data,
+                 std::string categoria,
+                 Morador* pagador,
+                 std::vector<Morador*> envolvidos);
+
+    void dividir() override;
+};
+
+#endif
 
